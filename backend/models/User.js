9b5461da3d4c6d7597102a6ad0ca30f61b43fe0a
Anashussain17 <anashussain1702@gmail.com>
2025-11-01 16:37:59 +0000

    Added email verification

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,10 +3,13 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
+    verifyToken: { type: String },
+  isVerified: { type: Boolean, default: false },
   isAdmin:{
     type:Boolean,
     default:false
   }
+
 });
 
 export default mongoose.model("User", userSchema);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import sendEmail from "../workers/sendEmail.js"
 import dotenv from "dotenv"
 dotenv.config()
 const router = express.Router();
@@ -19,17 +20,64 @@ router.post("/signup", async (req, res) => {
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(400).json({ message: "User already exists" });
 
+    const emailToken = (Math.random() + 1).toString(36).substring(2);
+
     const salt = await bcrypt.genSalt(10);
     const passwordHash = await bcrypt.hash(password, salt);
 
-    const newUser = new User({ email, passwordHash });
+    const newUser = new User({
+      email,
+      passwordHash,
+      verifyToken: emailToken
+    });
+
     await newUser.save();
 
-    res.json({ message: "Signup successful!✅" });
+    // Send Email
+    sendEmail({
+      to: email,
+      subject: "ReportMLA - Email Confirmation",
+      body: `
+        Hello,<br/>
+        Welcome to ReportMLA!<br/><br/>
+        Please verify your email:<br/><br/>
+        <a href="http://localhost:5717/api/auth/verify/${emailToken}">
+          ✅ Verify Email
+        </a><br/><br/>
+        Thank you,<br/>
+        <b>ReportMLA Team</b>
+      `,
+    });
+
+    res.json({ message: "Signup successful! Please verify your email 📧" });
+
   } catch (err) {
+    console.log(err); 
     res.status(500).json({ message: "Server error" });
   }
 });
+
+
+// Verify Email
+router.get("/verify/:token", async (req, res) => {
+  try {
+    const user = await User.findOne({ verifyToken: req.params.token });
+
+    if (!user) {
+      return res.status(400).send("<h3>Invalid or expired token ❌</h3>");
+    }
+
+    user.isVerified = true;
+    user.verifyToken = null;
+    await user.save();
+
+    res.send("<h3>Email Verified Successfully ✅ You can now login.</h3>");
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+});
+
+
 
 let adminEmails=["anas@anashussain.xyz"]
 // Login
@@ -41,6 +89,10 @@ router.post("/login", async (req, res) => {
     if (!user){
       console.log("❌ No account found for:", email);
       return res.status(404).json({ message: "No account found with this email.❌" });} 
+    if (!user.isVerified) {
+  return res.status(401).json({ message: "Please verify your email before logging in 📧" });
+}
+
 
     const isMatch = await bcrypt.compare(password, user.passwordHash);
     if (!isMatch) return res.status(401).json({ message: "Incorrect password ❌. Please try again." });
